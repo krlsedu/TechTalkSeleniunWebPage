671683639508bf36cf6a25b07f24950f2cb20ea3
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-10-14 00:10:51 -0300

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebPage';
+
+  techTalk: any = {}
+
+  techTalks: any[] = []
+
+  submitForm(formData: any): void {
+    console.log("submitForm");
+    console.log(formData);
+    console.log(this.techTalks);
+    this.techTalks.forEach((techTalk: any) => {
+        if (techTalk.title == formData.value.title) {
+          console.log("Duplicate");
+          formData.controls['title'].setErrors({'exists': true});
+          return;
+        }
+      }
+    );
+    if (formData.valid) {
+      console.log(formData.value);
+      this.techTalks.push(formData.value);
+      formData.reset();
+    }
+  }
 }
